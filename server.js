7eddcfddcae43b5dfc1c4acb5a7ee1b238635919
Biswajit Panda <biswajitpanda552@gmail.com>
2024-01-29 09:14:35 +0530

    day 11 (2)

--- server.js
+++ server.js
@@ -48,11 +48,10 @@ const upload = multer({ storage: storage });
 const __dirname1 = path.resolve();
 if(process.env.NODE_ENV==='production'){
 
-  app.use(express.static(path.join(__dirname1, '/build')))
+  app.use(express.static(path.join(__dirname1, '/client/build')))
 
   app.get('*', (req,res) => {
-    console.log("Serving HTML file");
-    res.sendFile(path.resolve(__dirname1, "build","index.html"))
+    res.sendFile(path.resolve(__dirname1, "client", "build","index.html"))
   });
 }else{
   app.get("/", (req,res) => {
