16e23f52cbbefc99ef3464c5d59adbef8e284298
Biswajit Panda <biswajitpanda552@gmail.com>
2024-01-29 19:13:46 +0530

    trying to deploy(18)

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -24,7 +24,7 @@ function App() {
     return () => {
       newSocket.disconnect();
     };
-  }, [serverUrl]);
+  }, []);
 
   useEffect(() => {
     if (user && socket) socket.emit("userConnected", user._id);
