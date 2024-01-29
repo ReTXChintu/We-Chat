739683840392c7657eacefb257a502c59064a81b
Biswajit Panda <biswajitpanda552@gmail.com>
2024-01-29 16:35:43 +0530

    trying to deploy(5)

--- Backend/server.js
+++ Backend/server.js
@@ -47,12 +47,12 @@ const upload = multer({ storage: storage });
 
 // -------------------------Deployment----------------------------
 
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  // app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+    res.send("Hellooo");
   });
 } else {
   app.get("/", (req, res) => {
