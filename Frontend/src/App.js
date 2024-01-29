a7446f4c1d1cd641e3b7dfaaeeb32a498f65a386
Biswajit Panda <biswajitpanda552@gmail.com>
2024-01-29 18:54:42 +0530

    trying to deploy(14)

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -7,7 +7,7 @@ import { Route, Routes } from "react-router-dom";
 
 function App() {
   const serverUrl = process.env.REACT_APP_BACKEND_URL;
-  const socket = io.connect(serverUrl);
+  let socket;
   const [user, setUser] = useState(
     JSON.parse(localStorage.getItem("weChatAppUser"))
   );
@@ -19,16 +19,23 @@ function App() {
     if (user) socket.emit("userConnected", user._id);
   }, [user, socket]);
 
+  useEffect(() => {
+    socket = io.connect(serverUrl);
+  }, []);
+
   return (
     <ChakraProvider>
       <Routes>
-        <Route path="/" element={user ? (
-            <Feed user={user} socket={socket} />
-          ) : (
-            <Login toggleUser={toggleUser} />
-          )}>
-          
-        </Route>
+        <Route
+          path="/"
+          element={
+            user ? (
+              <Feed user={user} socket={socket} />
+            ) : (
+              <Login toggleUser={toggleUser} />
+            )
+          }
+        ></Route>
       </Routes>
       {/* Add more routes if needed */}
     </ChakraProvider>
